4deefe70bf2e40025b4978b672b97fedb3839231
devi <devilakshmi@gmail.com>
2020-06-28 10:51:28 -0800

    added header and footer

---
+++ include/footer.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div class="footer"> 
+		copyright 2020 -All rights reserved by devi..
+	</div>
+
+</body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	
+   
+	
+	<div class="header">
+		 
+			<a href="#"><img src="images/images.png"></a>
+			<div class="header-menu">
+		<a href="#">about</a>
+		<a href="#">contact</a>
+		<a href="#">download</a>
+		<a href="#">service</a>
+		 <a href="#">home</a>
+		
+	</div>
+</div>
+</body>
+</html>
