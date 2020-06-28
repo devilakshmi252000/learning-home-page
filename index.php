069626f08c40613fdbfa0f93cbabcf86a6f369b0
devi <devilakshmi@gmail.com>
2020-06-28 10:45:22 -0800

    added index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>php project</title>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="icon" type="text/css" href="images/icon.png">
+</head>
+<body>
+     <?php
+     include 'include/header.php';
+     ?>
+ 
+     <img src="images/home.jpg">
+    <?php
+
+       include 'include/footer.php';
+    ?>
+
+</body>
+</html>
